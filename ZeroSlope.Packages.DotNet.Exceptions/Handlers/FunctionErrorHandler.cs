64ccd0bdd647922f5fd4acd90b177d1cc592eb3a
cfryerdev <cfryerdev@gmail.com>
2021-11-29 13:46:33 -0700

    Added shorthand handlers to start simplifying serverless, to be replaced

---
+++ ZeroSlope.Packages.DotNet.Exceptions/Handlers/FunctionErrorHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZeroSlope.Packages.DotNet.Exceptions.Handlers
+{
+    public static class FunctionErrorHandler
+    {
+        public static ObjectResult Handle(Exception ex)
+        {
+            var error = new HandledResult<Exception>(ex).HandleException();
+            var result = new ObjectResult(error);
+            result.StatusCode = StatusCodes.Status500InternalServerError;
+            return result;
+        }
+
+        public static ObjectResult Handle(HandledException ex)
+        {
+            var result = new ObjectResult(ex);
+            result.StatusCode = (int)ex.StatusCode;
+            return result;
+        }
+    }
+}
